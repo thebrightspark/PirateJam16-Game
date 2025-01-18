00a94c5bf345f092a9b351d78298103f0b0f14ca
bright_spark <markpassey@btinternet.com>
2025-01-18 23:16:37 +0000

    Added projectile spread and multishot, added temp player image

--- Levels/player_controller.gd
+++ Levels/player_controller.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D
 
 @export var max_health = 100
-@export var health = max_health
+@export var health = 100
 @export var speed = 150.0
 @export var jump_speed = 400.0
-@export var attack_cooldown_millis = 500
+
+@export_group("Attack")
+@export var cooldown_millis = 500
+@export var spread_deg = 0
+@export var multishot = 1
+@export var multishot_spread_deg = 5
 
 var projectile: PackedScene = preload("res://Projectile/projectile.tscn")
 var last_attack_timestamp = 0.0
@@ -34,12 +39,23 @@ func handle_movement(delta: float) -> void:
 
 func handle_attacking() -> void:
 	var time = Time.get_ticks_msec()
-	var is_after_cooldown = time > last_attack_timestamp + attack_cooldown_millis
+	var is_after_cooldown = time > last_attack_timestamp + cooldown_millis
 	if is_after_cooldown and Input.is_action_pressed("Click"):
 		last_attack_timestamp = time
 		var mouse_pos = get_global_mouse_position()
 		var look_vec = (mouse_pos - self.global_position).normalized()
-		var instance: Node2D = projectile.instantiate()
-		instance.global_position = self.global_position + (look_vec * 15)
-		instance.look_at(mouse_pos)
-		add_sibling(instance)
+		var multishot_rotation_max = -((multishot - 1) * multishot_spread_deg)
+		multishot_rotation_max /= 2
+		for m in multishot:
+			var instance: Node2D = projectile.instantiate()
+			instance.global_position = self.global_position + (look_vec * 15)
+			instance.look_at(mouse_pos)
+			var multishot_rotation = multishot_rotation_max + (m * multishot_spread_deg)
+			instance.rotate(deg_to_rad(multishot_rotation))
+			apply_projectile_modifiers(instance)
+			add_sibling(instance)
+
+func apply_projectile_modifiers(projectile: Node2D) -> void:
+	if spread_deg > 0:
+		var rotation_offset = (randf() * spread_deg) - (spread_deg / 2.0)
+		projectile.rotate(deg_to_rad(rotation_offset))
--- Projectile/projectile.gd
+++ Projectile/projectile.gd
@@ -8,17 +8,27 @@ extends RigidBody2D
 @export var homing = false
 @export var homing_range = 20
 
+var projectile: PackedScene = preload("res://Projectile/projectile.tscn")
+
 var age: float = 0
 var velocity = Vector2(0, -speed)
+
+# TODO: Rotate to direction of movement
 
 func _ready() -> void:
 	self.apply_central_impulse(velocity.rotated(global_rotation).rotated(deg_to_rad(90)))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	age += 1.0 * delta
-	if age >= lifetime:
-		queue_free()
+	handle_age(delta)
+
+func _physics_process(delta: float) -> void:
+	pass
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	pass
+
+func handle_age(delta: float) -> void:
+	age += 1.0 * delta
+	if age >= lifetime:
+		queue_free()
