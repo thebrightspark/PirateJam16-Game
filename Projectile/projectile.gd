bf6bd5932bf706e5ba213738640b5cb13cdb6d32
bright_spark <markpassey@btinternet.com>
2025-01-18 23:26:48 +0000

    Rotate projectile to direction of movement

--- Projectile/projectile.gd
+++ Projectile/projectile.gd
@@ -11,19 +11,16 @@ extends RigidBody2D
 var projectile: PackedScene = preload("res://Projectile/projectile.tscn")
 
 var age: float = 0
-var velocity = Vector2(0, -speed)
-
-# TODO: Rotate to direction of movement
 
 func _ready() -> void:
-	self.apply_central_impulse(velocity.rotated(global_rotation).rotated(deg_to_rad(90)))
+	self.apply_central_impulse(Vector2(0, -speed).rotated(global_rotation).rotated(deg_to_rad(90)))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	handle_age(delta)
 
 func _physics_process(delta: float) -> void:
-	pass
+	look_at(global_position + linear_velocity)
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	pass
