ff047fca2b598d44b4f2123eb8fc332f744179ee
bright_spark <markpassey@btinternet.com>
2025-01-30 00:54:26 +0000

    Right click projectiles now work

--- Levels/player_controller.gd
+++ Levels/player_controller.gd
@@ -20,7 +20,7 @@ class_name Player
 @onready var levitate_particles: GPUParticles2D = $LevitateParticles
 
 var projectile_scene: PackedScene = preload("res://Projectile/projectile.tscn")
-var last_attack_timestamp = 0.0
+var next_attack_timestamp = 0.0
 
 var levitating = false
 var levitate_amount = 0.0
@@ -68,25 +68,31 @@ func handle_movement(delta: float) -> void:
 
 func handle_attacking() -> void:
 	var time = Time.get_ticks_msec()
-	var is_after_cooldown = time > last_attack_timestamp + cooldown_millis
-	if is_after_cooldown and Input.is_action_pressed("Click"):
-		last_attack_timestamp = time
-		var mouse_pos = get_global_mouse_position()
-		var look_vec = (mouse_pos - self.global_position).normalized()
-		var multishot = data.modifiers_left.multishot + 1
-		var multishot_rotation_max = -((multishot - 1) * multishot_spread_deg)
-		multishot_rotation_max /= 2
-		for m in multishot:
-			var instance: Node2D = projectile_scene.instantiate()
-			instance.global_position = self.global_position + (look_vec * 15)
-			instance.look_at(mouse_pos)
-			var multishot_rotation = multishot_rotation_max + (m * multishot_spread_deg)
-			instance.rotate(deg_to_rad(multishot_rotation))
-			apply_projectile_modifiers(instance)
-			add_sibling(instance)
+	if time >= next_attack_timestamp:
+		if Input.is_action_pressed("Left Click"):
+			next_attack_timestamp = time + cooldown_millis - data.modifiers_left.cooldown
+			shoot(data.modifiers_left)
+		elif Input.is_action_pressed("Right Click"):
+			next_attack_timestamp = time + cooldown_millis - data.modifiers_right.cooldown
+			shoot(data.modifiers_right)
 
-func apply_projectile_modifiers(projectile: Node2D) -> void:
-	projectile.attributes = data.modifiers_left.create_projectile_attributes()
+func shoot(modifier_collection: ModifierCollection) -> void:
+	var mouse_pos = get_global_mouse_position()
+	var look_vec = (mouse_pos - self.global_position).normalized()
+	var multishot = modifier_collection.multishot + 1
+	var multishot_rotation_max = -((multishot - 1) * multishot_spread_deg)
+	multishot_rotation_max /= 2
+	for m in multishot:
+		var instance: Node2D = projectile_scene.instantiate()
+		instance.global_position = self.global_position + (look_vec * 15)
+		instance.look_at(mouse_pos)
+		var multishot_rotation = multishot_rotation_max + (m * multishot_spread_deg)
+		instance.rotate(deg_to_rad(multishot_rotation))
+		apply_projectile_modifiers(instance, modifier_collection)
+		add_sibling(instance)
+
+func apply_projectile_modifiers(projectile: Node2D, modifier_collection: ModifierCollection) -> void:
+	projectile.attributes = modifier_collection.create_projectile_attributes()
 	if spread_deg > 0:
 		var rotation_offset = (randf() * spread_deg) - (spread_deg / 2.0)
 		projectile.rotate(deg_to_rad(rotation_offset))
